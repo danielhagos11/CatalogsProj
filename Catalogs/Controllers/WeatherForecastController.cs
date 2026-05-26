94b92820ca00324042706f225a1a942ed06dcedb
danielhagos11 <138770966+danielhagos11@users.noreply.github.com>
2026-05-26 11:47:11 +0100

    Update weather summary to include new description

--- Catalogs/Controllers/WeatherForecastController.cs
+++ Catalogs/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ namespace Catalogs.Controllers
         private readonly ILogger<WeatherForecastController> _logger;
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Rainy"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","super duper rainyyy"
         };
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
